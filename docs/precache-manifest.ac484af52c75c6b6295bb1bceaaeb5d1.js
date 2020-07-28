632d603f726e3eff8b5bb3041f0ad38eb1a396b5
miriamschaefer <msuarezmelian@gmail.com>
2020-07-28 10:21:42 +0200

    run docs

---
+++ docs/precache-manifest.ac484af52c75c6b6295bb1bceaaeb5d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c034cdbcd3c8d4c16c2a92fbb78446f7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2ae150d17d51028a883c",
+    "url": "./static/css/main.98c69a58.chunk.css"
+  },
+  {
+    "revision": "a55c1ad030da411b7062",
+    "url": "./static/js/2.06d90b1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.06d90b1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ae150d17d51028a883c",
+    "url": "./static/js/main.77b18a39.chunk.js"
+  },
+  {
+    "revision": "7966156b2e5dae4af56d",
+    "url": "./static/js/runtime-main.0a0f8627.js"
+  }
+]);
