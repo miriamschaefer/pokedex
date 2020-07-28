8b36fa67479616ab070e7532fa69dd679038d07d
miriamschaefer <msuarezmelian@gmail.com>
2020-07-28 08:47:34 +0200

    set react

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../stylesheets/App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">Holi</header>
+      </div>
+    );
+  }
+}
+
+export default App;
